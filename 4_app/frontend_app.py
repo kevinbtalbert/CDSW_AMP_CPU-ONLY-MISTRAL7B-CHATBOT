4ec5a38702369d880091a1df3f406cbca0f4b937
Kevin Talbert <ktalbert@cloudera.com>
2023-10-03 21:50:47 -0400

    initial commit

---
+++ 4_app/frontend_app.py
@@ -0,0 +1,163 @@
+import os
+import gradio
+from milvus import default_server
+from pymilvus import connections, Collection
+from typing import Any, Union, Optional
+from pydantic import BaseModel
+import tensorflow as tf
+import utils.vector_db_utils as vector_db
+import utils.model_embedding_utils as model_embedding
+from llama_cpp import Llama
+
+## Initialize Llama2 Model on app startup
+model_path = "/home/cdsw/models/gen-ai-model/llama-2-13b-chat.ggmlv3.q5_1.bin"
+
+llama2_model = Llama(
+    model_path=model_path,
+    n_gpu_layers=64,
+    n_ctx=2000
+)
+
+
+# Test an inference
+print(llama2_model(prompt="Hello ", max_tokens=1))
+
+app_css = f"""
+        .gradio-header {{
+            color: white;
+        }}
+        .gradio-description {{
+            color: white;
+        }}
+
+        #custom-logo {{
+            text-align: center;
+        }}
+        .gr-interface {{
+            background-color: rgba(255, 255, 255, 0.8);
+        }}
+        .gradio-header {{
+            background-color: rgba(0, 0, 0, 0.5);
+        }}
+        .gradio-input-box, .gradio-output-box {{
+            background-color: rgba(255, 255, 255, 0.8);
+        }}
+        h1 {{
+            color: white; 
+            font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+            font-size: large; !important;
+        }}
+"""
+
+def main():
+    # Configure gradio QA app 
+    print("Configuring gradio app")
+    demo = gradio.Interface(fn=get_responses,
+                            title="Enterprise Custom Knowledge Base Chatbot with Llama2",
+                            description="This AI-powered assistant uses Cloudera DataFlow (NiFi) to scrape a website's sitemap and create a knowledge base. The information it provides as a response is context driven by what is available at the scraped websites. It uses Meta's open-source Llama2 model and the sentence transformer model all-mpnet-base-v2 to evaluate context and form an accurate response from the semantic search. It is fine tuned for questions stemming from topics in its knowledge base, and as such may have limited knowledge outside of this domain. As is always the case with prompt engineering, the better your prompt, the more accurate and specific the response.",
+                            inputs=[gradio.Radio(['llama-2-13b-chat'], label="Select Model", value="llama-2-13b-chat"), gradio.Radio(['1', '2', '3'], label="Select Temperature (Randomness of Response)", value=["1", "2", "3"]), gradio.Radio(["50", "100", "250", "500", "1000"], label="Select Number of Tokens (Length of Response)", value=["50", "100", "250", "500", "1000"]), gradio.Textbox(label="Topic Weight", placeholder="This field can be used to prioritize a topic weight."), gradio.Textbox(label="Question", placeholder="Enter your question here.")],
+                            outputs=[gradio.Textbox(label="Llama2 Model Response"), gradio.Textbox(label="Context Data Source(s)")],
+                            allow_flagging="never",
+                            css=app_css)
+
+
+    # Launch gradio app
+    print("Launching gradio app")
+    demo.launch(share=True,
+                enable_queue=True,
+                show_error=True,
+                server_name='127.0.0.1',
+                server_port=int(os.getenv('CDSW_APP_PORT')))
+    print("Gradio app ready")
+
+# Helper function for generating responses for the QA app
+def get_responses(engine, temperature, token_count, topic_weight, question):
+    if engine is "" or question is "" or engine is None or question is None:
+        return "One or more fields have not been specified."
+    if temperature is "" or temperature is None:
+      temperature = 1
+      
+    if topic_weight is "" or topic_weight is None:
+      topic_weight = None
+      
+    if token_count is "" or token_count is None:
+      token_count = 100
+
+    # Load Milvus Vector DB collection
+    vector_db_collection = Collection('cloudera_ml_docs')
+    vector_db_collection.load()
+    
+    # Phase 1: Get nearest knowledge base chunk for a user question from a vector db
+    if topic_weight: 
+        vdb_question = "Topic: " + topic_weight + " Question: " + question
+    else:
+        vdb_question = question
+    context_chunk, sources = get_nearest_chunk_from_vectordb(vector_db_collection, vdb_question)
+    vector_db_collection.release()
+
+    if engine == "llama-2-13b-chat":
+        # Phase 2a: Perform text generation with LLM model using found kb context chunk
+        response = get_llama2_response_with_context(question, context_chunk, temperature, token_count, topic_weight)
+
+    return response, sources
+
+# Get embeddings for a user question and query Milvus vector DB for nearest knowledge base chunk
+def get_nearest_chunk_from_vectordb(vector_db_collection, question):
+    # Generate embedding for user question
+    question_embedding =  model_embedding.get_embeddings(question)
+    
+    # Define search attributes for Milvus vector DB
+    vector_db_search_params = {"metric_type": "IP", "params": {"nprobe": 10}}
+    
+    # Execute search and get nearest vector, outputting the relativefilepath
+    nearest_vectors = vector_db_collection.search(
+        data=[question_embedding], # The data you are querying on
+        anns_field="embedding", # Column in collection to search on
+        param=vector_db_search_params,
+        limit=1, # limit results to 1 but allow for more via user customization
+        expr=None, 
+        output_fields=['relativefilepath'], # The fields you want to retrieve from the search result.
+        consistency_level="Strong"
+    )
+
+    # Return text of the nearest knowledgebase chunk
+    response = ""
+    sources = ""
+    for f in nearest_vectors[0]:
+        response += str(load_context_chunk_from_data(f.id))
+        sources += f.id
+    
+    return response, sources
+  
+# Return the Knowledge Base doc based on Knowledge Base ID (relative file path)
+def load_context_chunk_from_data(id_path):
+    with open(id_path, "r") as f: # Open file in read mode
+        return f.read()
+
+  
+# Pass through user input to LLM model with enhanced prompt and stop tokens
+def get_llama2_response_with_context(question, context, temperature, token_count, topic_weight):
+
+    if topic_weight is not None:
+        question = "Answer this question based on given context. If you do not know the answer, do not make something up. You should know that this question is about the topic " + str(topic_weight) + " This is the question: " + str(question)
+    else:
+        question = "Answer this question based on given context. If you do not know the answer, do not make something up. This is the question: " + str(question)
+    
+    question_and_context = question + "Here is the context: " + str(context)
+
+    try:
+        params = {
+            "temperature": float(temperature),
+            "max_tokens": int(token_count)
+        }
+        response = llama2_model(prompt=question_and_context, **params)
+
+        model_out = response['choices'][0]['text']
+        return model_out
+    
+    except Exception as e:
+        return "Error in generating response."
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# cdf-to-cdsw-llama2-chatbot-cdsw-compliant
-CDSW compliant version of CDF to CML Llama2 chatbot AMP
+# CDF to CML with LLama2 model
+Leverage the Llama2 model for creating a UI or API derived from your own knowledge base, scraped from your organization's website. This AMP contains the files to host an open source Llama2-based model and an accompanying UI or API. This AMP enables organizations to deploy a custom chatbot, currated to data scraped from a website (or websites) sitemap(s) using CDF (NiFi). 
+
+![](/assets/catalog-entry.png)
+
+## AMP Architecture
+![](/assets/architecture.png)
+
+
+## Building your custom knowledge base
+To build your own custom knowledge base, you will want to follow the instructions [here](USER_START_HERE/Build_Your_Own_Knowledge_Base_Tools/README.md) in the folder `USER_START_HERE`. There are guides for a Cloudera DataFlow and Pythonic implementation of how to do this. Then, you will want to rerun the `Populate Vector DB` Job to ensure your vector DB has the latest embeddings.
+
+## Two Flavors: UI (Front end) and API
+This project allows you to access the context-driven LLM using two flavors: a UI and an API. Both are listed as Applications in CML which you can spin up/down as needed. 
+
+### UI (Front end)
+
+This is the default application choice for the AMP. You should be able to access the view through your applications nav. When it starts, you will be able to select the default model (`llama-2-13b-chat`), temperature (a good default is 1), number of tokens (a good default may be 100), topic weight (a domain for the corpus of knowledge to prioritize), and question for the model to process. Defaults will be selected if you choose not to answer these; however a question is required.
+
+![](/assets/interface.png)
+
+### REST API
+
+#### Forming a request to the API
+Requests can be formed intra-domain or cross-domain. For cross-domain requests, you'll need to ensure unauthenticated app access is allowed for the POST endpoint to be reachable. Be cognizant of the amount of tokens and temperature you feed into the payload parameters. Most requests for a couple sentences should use around 200 tokens, a paragraph could use upwards of 600-800. 
+
+![](/assets/unauthenticated-access1.png)
+
+Note that if this option does not appear, you may need to update your Administrative security settings to mirror the below, where "Allow applications to be configured with unauthenticated access" is checked:
+
+![](/assets/unauthenticated-access2.png)
+
+For request syntax: 
+GET and POST to the ROOT of the CML application endpoint (e.g. `https://subdomain.domain.go01-dem.ylcu-atmi.cloudera.site/`)
+Successful GET request should indicate the API is up and running:
+
+![](/assets/GET-endpoint.png)
+
+Forming the POST request can be done through Postman or natively in CML:
+
+1. Postman
+
+Form the payload/url and body to match the below, and add the header `Content-Type | application/json`
+
+```
+{
+    "inputs": "What is Cloudera Data Science Workbench?",
+    "parameters": {
+        "temperature": 1,
+        "max_tokens": 100
+    }
+}
+```
+
+![](/assets/postman-setup.png)
+
+Note that in future development, `engine` may also be customized to include more than the Llama2 one which comes with the AMP deployment.
+
+2. Pythonic (Available in the 4_app folder as an Jupyter notebook)
+
+## Requirements
+#### CML Instance Types
+- A GPU instance is required to perform inference on the LLM
+  - [CML Documentation: GPUs](https://docs.cloudera.com/machine-learning/cloud/gpu/topics/ml-gpu.html)
+- A CUDA 5.0+ capable GPU instance type is recommended
+  - The torch libraries in this AMP require a GPU with CUDA compute capability 5.0 or higher. (i.e. nVidia V100, A100, T4 GPUs)
+
+#### Resource Requirements
+This AMP creates the following workloads with resource requirements:
+- CML Session: `2 CPU, 4GB MEM`
+- CML Jobs: `4 CPU, 16GB MEM`
+- CML Application: `2 CPU, 1 GPU, 16GB MEM`
+
+#### External Resources
+This AMP requires pip packages and models from huggingface. Depending on your CML networking setup, you may need to whitelist some domains:
+- pypi.python.org
+- pypi.org
+- pythonhosted.org
+- huggingface.co
+
+## Technologies Used
+#### Open-Source Models and Utilities
+- [all-mpnet-base-v2](https://huggingface.co/sentence-transformers/sentence-transformers/all-mpnet-base-v2/resolve/main/all-mpnet-base-v2.tar.gz)
+     - Vector Embeddings Generation Model
+- [llama-2-13b-chat.ggmlv3.q5_1](https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q5_1.bin)
+   - Instruction-following Large Language Model
+- [Hugging Face transformers library](https://pypi.org/project/transformers/)
+#### Vector Database
+- [Milvus](https://github.com/milvus-io/milvus)
+#### Chat Frontend
+- [Gradio](https://github.com/gradio-app/gradio)
+#### Chat API
+- [FastAPI](https://fastapi.tiangolo.com/)
+
+## Deploying on CML
+There are two ways to launch this prototype on CML:
+
+1. **From Prototype Catalog** - Navigate to the Prototype Catalog on a CML workspace, select the "CDF to CML with Llama2 models" tile, click "Launch as Project", click "Configure Project".
+2. **As ML Prototype** - In a CML workspace, click "New Project", add a Project Name, select "ML Prototype" as the Initial Setup option, copy in the [repo URL](https://github.com/kevinbtalbert/cdf-to-cml-llama2-chatbot), click "Create Project", click "Configure Project".
+
+
+## The Fine Print
+
+All the components of the application (knowledge base, context retrieval, prompt enhancement LLM) are running within CDF and CML. This application does not call any external model APIs nor require any additional training of an LLM. The knowledge base is generated using the user passed sitemaps in NiFi (CDF) or Python, depending on the user preference.
+
+By configuring and launching this AMP, you will cause TheBloke/Llama-2-13B-chat-GGML, which is a third party large language model (LLM), to be downloaded and installed into your environment from the third party’s website. Additionally, you will be downloading sentence-transformers/all-mpnet-base-v2, which is the embedding model used in this project. Please see https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML and https://huggingface.co/sentence-transformers/all-mpnet-base-v2 for more information about the LLM and embedding model, including the applicable license terms.  If you do not wish to download and install TheBloke/Llama-2-13B-chat-GGML and sentence-transformers/all-mpnet-base-v2, do not deploy this repository.  By deploying this repository, you acknowledge the foregoing statement and agree that Cloudera is not responsible or liable in any way for TheBloke/Llama-2-13B-chat-GGML and sentence-transformers/all-mpnet-base-v2. Author: Cloudera Inc.
